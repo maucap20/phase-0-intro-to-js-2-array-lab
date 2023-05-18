6e3085d8cd7dcf812621f33b430189bf968285f1
Adam Mauch <maucap20@wfu.edu>
2023-05-18 09:44:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name){
+    let copyCats = [...cats];
+    copyCats.push(name);
+    return copyCats;
+}
+function prependCat(name) {
+    let copyCats = [...cats];
+    copyCats.unshift(name);
+    return copyCats;
+}
+function removeLastCat() {
+    let copyCats = [...cats];
+    copyCats.pop();
+    return copyCats;
+}
+function removeFirstCat() {
+    let copyCats = [...cats];
+    copyCats.shift();
+    return copyCats;
+}
